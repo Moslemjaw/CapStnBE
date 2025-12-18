a832b031413240ac3ee462a330d88e962e7acf66
alialarbash <ali.alarbash456@gmail.com>
2025-12-18 13:11:10 +0300

    tested all apis and everything works, but authorization is not in many of them and a lot of validations need to be done

--- src/api/Survey/survey.router.ts
+++ src/api/Survey/survey.router.ts
@@ -20,11 +20,11 @@ surveyRouter.get("/published", getPublishedSurveys);
 surveyRouter.get("/unpublished", getUnpublishedSurveys);
 surveyRouter.get("/:id", getSurveyById);
 // :ater check if the user is the creator of the survey for delete and update
+surveyRouter.put("/publish/:id", authorize, publishSurvey);
+surveyRouter.put("/unpublish/:id", authorize, unpublishSurvey);
 surveyRouter.put("/:id", updateSurvey);
 surveyRouter.delete("/:id", deleteSurvey);
 // Protected routes – require a valid JWT so we can set req.user.id as creatorId
 surveyRouter.post("/", authorize, createSurvey);
-surveyRouter.post("/publish/:id", authorize, publishSurvey);
-surveyRouter.post("/unpublish/:id", authorize, unpublishSurvey);
 
 export default surveyRouter;
